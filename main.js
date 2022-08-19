4757eaeea5a9c8a55001ce079f8386dbe2746454
Dmitry Stoyan <jombee@yandex.ru>
2022-08-19 21:22:05 +0300

    JS popup + profile

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Закрытие и открытие модального окна
+
 const openPopUp = document.querySelector('.profile__edit-button');
 const closePopUp = document.querySelector('.popup__close-button');
 const popUp = document.querySelector('.popup');
@@ -12,11 +14,28 @@ closePopUp.addEventListener('click', () => {
 
 
 
+// Изменение страницы через PopUp
 
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input-name');
 const jobInput = document.querySelector('.popup__input-job');
+const popupSave = document.querySelector('.popup__save-button');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
+
+  let profileTitle = document.querySelector('.profile__title');
+  let profileSubitle = document.querySelector('.profile__subtitle');
+
+  profileTitle.textContent = nameInput.value;
+  profileSubitle.textContent = jobInput.value;
+
+  popupSave.addEventListener('click', () => {
+    popUp.classList.remove('active');
+  })
 }
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
